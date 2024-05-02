dc4a0dd78273da3e979a71a67c7e6f24acfcc0f2
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-05-02 17:29:13 +0500

    ints to str in api

--- apps/course/serializer.py
+++ apps/course/serializer.py
@@ -2,15 +2,50 @@ from rest_framework import serializers
 from .models import Exam, Schedule, Subject, Course, Room
 
 
-
 class ExamSerializer(serializers.ModelSerializer):
     subject = serializers.SerializerMethodField()
+    room = serializers.SerializerMethodField()
+    test_choises = serializers.SerializerMethodField()
+    exam_choises = serializers.SerializerMethodField()
+
     class Meta:
         model = Exam
-        fields = ['id', 'subject', 'room', 'date', 'time_start', 'time_end', 'test_choises', 'exam_choises']
+        fields = ['id', 'subject', 'room', 'date', 'time_start', 'time_end',
+                  'test_choises', 'exam_choises']
 
-    def get_subject(self,obj):
+    def get_subject(self, obj):
         return obj.subject.name
+
+    def get_room(self, obj):
+        return obj.room.name
+
+    def get_test_choises(self, obj):
+        return dict(Exam._meta.get_field('test_choises').choices).get(obj.test_choises)
+
+    def get_exam_choises(self, obj):
+        return dict(Exam._meta.get_field('exam_choises').choices).get(obj.exam_choises)
+
+
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    teacher = serializers.SerializerMethodField()
+    subject = serializers.SerializerMethodField()
+    room = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Schedule
+        fields = ['id', 'teacher',  'subject', 'room', 'date', 'time_start', 'time_end']
+
+    def get_teacher(self, obj):
+        return obj.teacher.name if obj.teacher else None
+
+    def get_subject(self, obj):
+        return obj.subject.name if obj.subject else None
+
+    def get_room(self, obj):
+        return obj.room.name if obj.room else None
+
+
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -18,18 +53,13 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ['id', 'name']
 
-class ScheduleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Schedule
-        fields = ['id', 'teacher', 'subject', 'room', 'date', 'time_start', 'time_end']
-
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'name']
 
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'name', 'groups']
-
